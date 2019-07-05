21d3fa83f25ef37826acad8526d13cde76cc0ad3
koichi3298 <52521668+koichi3298@users.noreply.github.com>
2019-07-05 17:35:50 +0800

    Add files via upload

--- portfolio/labs/lab01/lab.js
+++ portfolio/labs/lab01/lab.js
@@ -4,9 +4,41 @@
 
 
 function start(e) {
+    var name = [
+       {
+        char: '劉',
+        big5: 'BC42 ',
+        cns:  '1-6C2C',
+        unicode:'5289'
 
+       },
+       {
+        char: '廣',
+        big5: 'BC73',
+        cns:  '1-6C5D',
+        unicode:'5EE3'
+       },
+       {
+        char: '義',
+        big5: 'B871 ',
+        cns:  '1-6578',
+        unicode:'7FA9'
+       }
+    ];
+
+    var ele = document.getElementById("div1");
+
+    for (var i = 0; i < 3; i++) {
+        ele.innerHTML += name[i].char;
+        ele.innerHTML += ' big5 =';
+        ele.innerHTML += name[i].big5;
+        ele.innerHTML += ' cns =';
+        ele.innerHTML += name[i].cns;
+        ele.innerHTML += ' unicode = ';
+        ele.innerHTML += name[i].unicode;
+        ele.innerHTML += '<br>';
+    }
 }
-
 /*
 https://www.w3schools.com/jsref/met_document_addeventlistener.asp
 
